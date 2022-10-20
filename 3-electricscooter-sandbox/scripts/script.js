925dac9f19d31f927ce548d0b2423ba9a83cea1a
ryzenkz <96327924+ryzenkz@users.noreply.github.com>
2022-10-20 13:08:31 +0600

    Add files via upload

---
+++ 3-electricscooter-sandbox/scripts/script.js
@@ -0,0 +1,19 @@
+arrowTop.onclick = function() {
+  window.scrollTo(pageXOffset, 0);
+  // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
+};
+
+window.addEventListener('scroll', function() {
+    arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+  });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
